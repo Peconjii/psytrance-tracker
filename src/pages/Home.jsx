fb191a12232d076079931a43ee3d12d6c0de0476
Peconjii <vlku.sr34.2025@uns.ac.rs>
2026-06-16 13:56:16 +0200

    Home minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom'
+import heroImg from '../images/hero.jpg'
+
 function Home() {
-    return <h1>Home</h1>
-}
+    return (
+        <div style={{ backgroundImage: `url(${heroImg})` }} className="relative h-screen bg-cover bg-center ">
+            <div className="absolute inset-0 bg-black opacity-50 flex flex-col items-center justify-center">
+            <h1 className='text-white text-5xl text-center p-4'>PSYTRANCE EVENT TRACKER</h1>
+            <div className='flex-box text-center justify-center p-16'>
+                <p className='text-white text-3xl text-center p-16'>Where bass meets the cosmos,<br></br> Find your tribe and feel the frequency.</p>
+                <Link to="/events" className='bg-purple-700 hover:bg-purple-500 text-white text-5xl justify-center items-center px-8 py-4 rounded-xl'>Explore Events</Link>
+            </div>
+        </div>
+    </div>
+)}
 
 export default Home
